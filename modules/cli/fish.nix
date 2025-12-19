c4252af6c02236f2cbf96868a81f5845a70147a2
Luytan <luytan@khora.me>
2025-12-19 11:58:54 +0100

    font + fish

--- modules/cli/fish.nix
+++ modules/cli/fish.nix
@@ -8,8 +8,7 @@
       zoxide init fish | source
     '';
     shellAliases = {
-      rebuild = "sudo nixos-rebuild switch --flake ~/Documents/NixOS/dotfiles#glaceon";
-      rebuild-sylv = "sudo nixos-rebuild switch --flake ~/Documents/NixOS/dotfiles#sylveon";
+      rebuild = "sudo nixos-rebuild switch --flake ~/Documents/NixOS/dotfiles#$(hostname)";
       upd = "nix flake update --flake ~/Documents/NixOS/dotfiles";
       clean = "nix-collect-garbage -d";
       cd = "z";
--- modules/common/fonts.nix
+++ modules/common/fonts.nix
@@ -2,6 +2,8 @@
 
 {
   fonts.packages = with pkgs; [
+    inter 
+    jetbrains-mono
     nerd-fonts.fira-code
     nerd-fonts.hack
     nerd-fonts.ubuntu
@@ -12,4 +14,30 @@
     fira-code
     fira-code-symbols
   ];
+
+
+  fonts.fontconfig = {
+    enable = true;
+    antialias = true;
+
+    hinting = {
+      enable = true;
+      style = "slight";
+      autohint = false;
+    };
+
+    subpixel = {
+      rgba = "rgb";
+      lcdfilter = "default";
+    };
+
+    defaultFonts = {
+      monospace = [ "JetBrains Mono" ];
+      sansSerif = [ "Inter" ];
+      serif = [ "Noto Serif" "Source Serif Pro" ];
+      emoji = [ "Noto Color Emoji" ];
+    };
+    allowBitmaps = false;
+    useEmbeddedBitmaps = false;
+  };
 }

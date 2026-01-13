9cff73b00f6b7a99dcbdbaf44ccc17e3f020ada7
Luytan <luytan@khora.me>
2026-01-13 15:05:38 +0100

    format flake

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-cisco.url = "github:NixOS/nixpkgs/pull/456650/head";
-    
+
     # Home Manager
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
@@ -25,78 +25,101 @@
       url = "github:Gerg-L/spicetify-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    noctalia = {
+      url = "github:noctalia-dev/noctalia-shell";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
+
   };
 
-  outputs = inputs@{ self, nixpkgs, lanzaboote, nixpkgs-unstable, nixpkgs-cisco, disko, home-manager, spicetify-nix, ... }: {
-    nixosConfigurations.glaceon = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./hosts/glaceon/configuration.nix
-        
-        disko.nixosModules.disko
-        lanzaboote.nixosModules.lanzaboote
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.extraSpecialArgs = {
-            inherit inputs;
-            pkgs-unstable = import nixpkgs-unstable {
-              system = "x86_64-linux";
-              config.allowUnfree = true;
-            };
-            pkgs-cisco = import nixpkgs-cisco {
-              system = "x86_64-linux";
-              config.allowUnfree = true;
-            };
-          };
-          home-manager.users.luytan = import ./home.nix;
-        }
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      lanzaboote,
+      nixpkgs-unstable,
+      nixpkgs-cisco,
+      disko,
+      home-manager,
+      spicetify-nix,
+      ...
+    }:
+    {
+      nixosConfigurations.glaceon = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/glaceon/configuration.nix
 
-        ({ pkgs, lib, ... }: {
-          boot.loader.systemd-boot.enable = lib.mkForce false;
-          boot.lanzaboote = {
-            enable = true;
-            pkiBundle = "/var/lib/sbctl";
-          };
-          environment.systemPackages = [ pkgs.sbctl ];
-        })
-      ];
-    };
-    nixosConfigurations.sylveon = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./hosts/sylveon/configuration.nix
-        
-        disko.nixosModules.disko
-        lanzaboote.nixosModules.lanzaboote
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.extraSpecialArgs = {
-            inherit inputs;
-            pkgs-unstable = import nixpkgs-unstable {
-              system = "x86_64-linux";
-              config.allowUnfree = true;
+          disko.nixosModules.disko
+          lanzaboote.nixosModules.lanzaboote
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = {
+              inherit inputs;
+              pkgs-unstable = import nixpkgs-unstable {
+                system = "x86_64-linux";
+                config.allowUnfree = true;
+              };
+              pkgs-cisco = import nixpkgs-cisco {
+                system = "x86_64-linux";
+                config.allowUnfree = true;
+              };
             };
-            pkgs-cisco = import nixpkgs-cisco {
-              system = "x86_64-linux";
-              config.allowUnfree = true;
-            };
-          };
-          home-manager.users.luytan = import ./home.nix;
-        }
+            home-manager.users.luytan = import ./home.nix;
+          }
 
-        ({ pkgs, lib, ... }: {
-          boot.loader.systemd-boot.enable = lib.mkForce false;
-          boot.lanzaboote = {
-            enable = true;
-            pkiBundle = "/var/lib/sbctl";
-          };
-          environment.systemPackages = [ pkgs.sbctl ];
-        })
-      ];
+          (
+            { pkgs, lib, ... }:
+            {
+              boot.loader.systemd-boot.enable = lib.mkForce false;
+              boot.lanzaboote = {
+                enable = true;
+                pkiBundle = "/var/lib/sbctl";
+              };
+              environment.systemPackages = [ pkgs.sbctl ];
+            }
+          )
+        ];
+      };
+      nixosConfigurations.sylveon = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/sylveon/configuration.nix
+
+          disko.nixosModules.disko
+          lanzaboote.nixosModules.lanzaboote
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = {
+              inherit inputs;
+              pkgs-unstable = import nixpkgs-unstable {
+                system = "x86_64-linux";
+                config.allowUnfree = true;
+              };
+              pkgs-cisco = import nixpkgs-cisco {
+                system = "x86_64-linux";
+                config.allowUnfree = true;
+              };
+            };
+            home-manager.users.luytan = import ./home.nix;
+          }
+
+          (
+            { pkgs, lib, ... }:
+            {
+              boot.loader.systemd-boot.enable = lib.mkForce false;
+              boot.lanzaboote = {
+                enable = true;
+                pkiBundle = "/var/lib/sbctl";
+              };
+              environment.systemPackages = [ pkgs.sbctl ];
+            }
+          )
+        ];
+      };
     };
-  };
 }

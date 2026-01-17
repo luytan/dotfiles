f2ecdeb2affee6e3ac96d6393e3f093e9d88c7d0
Luytan <luytan@khora.me>
2026-01-17 16:14:51 +0100

    new pipewire with low latency wow

--- modules/system/hardware/audio.nix
+++ modules/system/hardware/audio.nix
@@ -1,4 +1,9 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   cfg = config.modules.hardware.audio;
 in
@@ -11,9 +16,16 @@ in
   config = lib.mkIf cfg.enable {
     services.pipewire = {
       enable = true;
-      pulse.enable = true;
+      wireplumber.enable = true;
       alsa.enable = true;
       alsa.support32Bit = true;
+      pulse.enable = true;
+      jack.enable = true;
+      lowLatency = {
+        enable = true;
+        quantum = 64;
+        rate = 48000;
+      };
     };
     environment.systemPackages = with pkgs; [
       easyeffects

23f7555f110549972878eef7d90969dbff57ba56
Luytan <luytan@khora.me>
2026-01-31 08:39:58 +0100

    format + tmpfs for nix rebuild

--- modules/system/core/boot.nix
+++ modules/system/core/boot.nix
@@ -1,11 +1,17 @@
 { pkgs, ... }:
 {
-  boot.kernelPackages = pkgs.linuxPackages_6_18;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.initrd.systemd.enable = true;
-  boot.plymouth = {
-    enable = true;
-    theme = "bgrt";
+  boot = {
+    kernelPackages = pkgs.linuxPackages_6_18;
+    loader = {
+      efi.canTouchEfiVariables = true;
+      systemd-boot.consoleMode = "max";
+    };
+    initrd.systemd.enable = true;
+    plymouth = {
+      enable = true;
+      theme = "bgrt";
+    };
+    tmp.useTmpfs = true;
   };
   hardware.enableAllFirmware = true;
   hardware.cpu.amd.updateMicrocode = true;

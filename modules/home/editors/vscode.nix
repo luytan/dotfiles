3cc1bc79b119da46300b4750ad2d13e3733b286b
Luytan <luytan@khora.me>
2026-02-08 18:39:06 +0100

    started vscode config

--- modules/home/editors/vscode.nix
+++ modules/home/editors/vscode.nix
@@ -5,6 +5,12 @@
     package = pkgs-unstable.vscode;
     profiles.default.extensions = with pkgs.vscode-extensions; [
       catppuccin.catppuccin-vsc
+      pkief.material-icon-theme
+      jnoortheen.nix-ide
     ];
+    profiles.default.userSettings = {
+      "workbench.colorTheme" = "Catppuccin Mocha";
+      "workbench.iconTheme" = "material-icon-theme";
+    };
   };
 }

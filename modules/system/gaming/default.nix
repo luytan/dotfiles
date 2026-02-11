574b770542ba0627aeb665e2f2f6f0d40e0f89be
Luytan <luytan@khora.me>
2026-02-11 22:03:36 +0100

    gamemode cuz why not

--- modules/system/gaming/default.nix
+++ modules/system/gaming/default.nix
@@ -19,7 +19,7 @@ in
       programs.steam = {
         enable = true;
       };
-
+      programs.gamemode.enable = true;
       environment.systemPackages = with pkgs; [
         mangohud
       ];

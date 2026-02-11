5a8800b4adb45a94f81edb9d9fe7f0d3e1f39434
Luytan <luytan@khora.me>
2026-02-11 22:30:09 +0100

    vscodium now

--- modules/home/editors/vscode.nix
+++ modules/home/editors/vscode.nix
@@ -2,15 +2,25 @@
 {
   programs.vscode = {
     enable = true;
-    package = pkgs.vscode;
+    package = pkgs.vscodium;
     profiles.default.extensions = with pkgs.vscode-extensions; [
       catppuccin.catppuccin-vsc
       pkief.material-icon-theme
       jnoortheen.nix-ide
+      vscodevim.vim
+      ms-azuretools.vscode-containers
+      ms-python.python
+      ms-vscode.cpptools
+      esbenp.prettier-vscode
+      redhat.vscode-yaml
+      rust-lang.rust-analyzer
     ];
     profiles.default.userSettings = {
       "workbench.colorTheme" = "Catppuccin Macchiato";
       "workbench.iconTheme" = "material-icon-theme";
+      "update.enableWindowsBackgroundUpdates" = "false";
+      "update.mode" = "manual";
+      "extensions.autoUpdate" = "false";
     };
   };
 }

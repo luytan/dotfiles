c3df8aa52b4f8f0429e377c876fa261f2020fdf4
Luytan <luytan@khora.me>
2026-02-13 21:34:02 +0100

    latest gemini cli

--- modules/home/cli/gemini.nix
+++ modules/home/cli/gemini.nix
@@ -1,8 +1,8 @@
-{ pkgs, ... }:
+{ pkgs, pkgs-unstable, ... }:
 {
   programs.gemini-cli = {
     enable = true;
-
+    package = pkgs-unstable.gemini-cli;
     settings = {
       general = {
         preferredEditor = "vim";

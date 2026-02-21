cb9dc08bcf80487ea7baf3cd39ed891af2015514
Luytan <luytan@khora.me>
2026-02-21 12:42:17 +0100

    Renamed resolved to the new options

--- modules/system/hardware/network/network.nix
+++ modules/system/hardware/network/network.nix
@@ -21,13 +21,17 @@ in
 
     services.resolved = {
       enable = true;
-      dnssec = "true";
-      domains = [ "~." ];
-      fallbackDns = [
-        "1.1.1.1#one.one.one.one"
-        "1.0.0.1#one.one.one.one"
-      ];
-      dnsovertls = "opportunistic";
+      settings = {
+        Resolve = {
+          DNSSEC = true;
+          DNSOverTLS = "opportunistic";
+          Domains = [ "~." ];
+          FallbackDNS = [
+            "1.1.1.1#one.one.one.one"
+            "1.0.0.1#one.one.one.one"
+          ];
+        };
+      };
     };
     networking.firewall = {
       enable = true;

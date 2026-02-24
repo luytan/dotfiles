0f5ba8afb8751503b2e0dc4678f5ca0ac109c12c
Luytan <luytan@khora.me>
2026-02-24 15:20:23 +0100

    gamescope desktop entry

--- modules/home/gaming/default.nix
+++ modules/home/gaming/default.nix
@@ -2,7 +2,19 @@
 {
   home.packages = with pkgs; [
     mangohud
+    goverlay
     prismlauncher
     lutris
   ];
+  xdg.desktopEntries = {
+    gamescope = {
+      name = "Gamescope";
+      genericName = "Steam Big Picture";
+      type = "Application";
+      icon = "steam";
+      exec = "gamescope -W 2560 -H 1440 -r 120 --adaptive-sync -f -e -- steam -gamepadui";
+      terminal = false;
+      categories = ["Game"];
+    };
+  };
 }

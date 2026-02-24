4f6d6a8c469114c59d9cfac9be18f8620509acf7
Luytan <luytan@khora.me>
2026-02-24 19:09:52 +0100

    Removed some IDE i wont use

--- modules/home/dev/jetbrain.nix
+++ modules/home/dev/jetbrain.nix
@@ -4,16 +4,10 @@
     jetbrains.rust-rover
     jetbrains.goland
     jetbrains.clion
-    jetbrains.idea
     jetbrains-runner
-    jetbrains.pycharm
-    jetbrains.webstorm
   ];
-  home.file.".config/JetBrains/RustRover2025.3/rustrover64.vmoptions".text = "-Dawt.toolkit.name=WLToolkit";
+  home.file.".config/JetBrains/RustRover2025.3/rustrover64.vmoptions".text =
+    "-Dawt.toolkit.name=WLToolkit";
   home.file.".config/JetBrains/GoLand2025.3/goland64.vmoptions".text = "-Dawt.toolkit.name=WLToolkit";
   home.file.".config/JetBrains/CLion2025.3/clion64.vmoptions".text = "-Dawt.toolkit.name=WLToolkit";
-  home.file.".config/JetBrains/IntelliJIdea2025.3/idea64.vmoptions".text = "-Dawt.toolkit.name=WLToolkit";
-  home.file.".config/JetBrains/PyCharm2025.3/pycharm64.vmoptions".text = "-Dawt.toolkit.name=WLToolkit";
-  home.file.".config/JetBrains/WebStorm2025.3/webstorm64.vmoptions".text = "-Dawt.toolkit.name=WLToolkit";
-
 }

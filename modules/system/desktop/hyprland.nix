f6751cc277c8314f3d42b032c756c15295848fb9
Luytan <luytan@khora.me>
2026-03-13 19:55:46 +0100

    hyprland: Added UWSM support

--- modules/system/desktop/hyprland.nix
+++ modules/system/desktop/hyprland.nix
@@ -12,6 +12,7 @@ in
   config = lib.mkIf cfg.hyprland {
     programs.hyprland = {
       enable = true;
+      withUWSM = true;
       package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
       portalPackage =
         inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
@@ -21,6 +22,18 @@ in
       extraPortals = [
         pkgs.xdg-desktop-portal-gtk
       ];
+    };
+    environment.systemPackages = with pkgs; [
+      hyprpicker
+      app2unit
+      xdg-terminal-exec
+      playerctl
+    ];
+    environment = {
+      sessionVariables.APP2UNIT_SLICES = "a=app-graphical.slice b=background-graphical.slice s=session-graphical.slice";
+      sessionVariables.QT_QPA_PLATFORMTHEME = "qtengine";
+      sessionVariables.QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+      sessionVariables.QT_AUTO_SCREEN_SCALE_FACTOR = "1";
     };
   };
 }

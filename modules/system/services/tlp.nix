252f2c8e4fa201e1bc6db609b0fd6689675de2a3
Luytan <luytan@khora.me>
2026-03-20 18:18:30 +0100

    TLP: Added powertop, pd and more settings for my laptop

--- modules/system/services/tlp.nix
+++ modules/system/services/tlp.nix
@@ -9,23 +9,23 @@ let
 in
 {
   config = lib.mkIf cfg.tlp {
-
+    powerManagement.powertop.enable = true;
     services.system76-scheduler.settings.cfsProfiles.enable = true;
     services.power-profiles-daemon.enable = false;
     services.thermald.enable = config.hardware.cpu.intel.updateMicrocode;
     services.tlp = {
       enable = true;
       settings = {
-        CPU_BOOST_ON_AC = 0;
+        CPU_BOOST_ON_AC = 1;
         CPU_BOOST_ON_BAT = 0;
         CPU_HWP_DYN_BOOST_ON_AC = 1;
-        CPU_HWP_DYN_BOOST_ON_BAT = 1;
+        CPU_HWP_DYN_BOOST_ON_BAT = 0;
         CPU_SCALING_GOVERNOR_ON_AC = "performance";
         CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
         CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-        CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
         PLATFORM_PROFILE_ON_AC = "performance";
-        PLATFORM_PROFILE_ON_BAT = "balanced";
+        PLATFORM_PROFILE_ON_BAT = "quiet";
         START_CHARGE_THRESH_BAT0 = 75;
         STOP_CHARGE_THRESH_BAT0 = 81;
         RUNTIME_PM_ON_AC = "auto";
@@ -37,6 +37,9 @@ in
       // lib.optionalAttrs (config.networking.hostName == "sylveon") {
         # Specific to workstation
         CPU_BOOST_ON_AC = 1;
+      };
+      pd = {
+        enable = true;
       };
     };
   };

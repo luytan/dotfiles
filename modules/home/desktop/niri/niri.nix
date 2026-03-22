15310bdcaa56ce5a45ca966d7dbcd035df12ed1e
Luytan <luytan@khora.me>
2026-03-22 13:41:31 +0100

    Niri: More xdg portal setting

--- modules/home/desktop/niri/niri.nix
+++ modules/home/desktop/niri/niri.nix
@@ -18,6 +18,13 @@
     wlsunset
     nautilus
   ];
+  xdg.portal = {
+    enable = true;
+    xdgOpenUsePortal = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+    configPackages = [ pkgs.xdg-desktop-portal-gtk ];
+    config.common.default = "gtk";
+  };
   services.hypridle = {
     enable = true;
     settings = {

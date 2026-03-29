ec64e900b945c736453276f9c3e0b529edee15b6
Luytan <luytan@khora.me>
2026-03-29 16:52:14 +0200

    Snapper: Check if btrfs /home

--- modules/system/services/snapper.nix
+++ modules/system/services/snapper.nix
@@ -9,21 +9,31 @@ let
   cfg = config.modules.services;
 in
 {
-  config = lib.mkIf cfg.snapper {
-    services.snapper = {
-      configs.home = {
-        SUBVOLUME = "/home";
-        ALLOW_USERS = [ "${user}" ];
-        TIMELINE_CREATE = true;
-        TIMELINE_CLEANUP = true;
-        TIMELINE_LIMIT_HOURLY = "10";
-        TIMELINE_LIMIT_DAILY = "10";
-        TIMELINE_LIMIT_WEEKLY = "5";
-        TIMELINE_LIMIT_MONTHLY = "1";
+  config = lib.mkMerge [
+    (lib.mkIf cfg.snapper {
+      assertions = [
+        {
+          assertion = (config.fileSystems."/home".fsType or null) == "btrfs";
+          message = "modules.services.snapper requires /home to use btrfs.";
+        }
+      ];
+    })
+    (lib.mkIf (cfg.snapper && ((config.fileSystems."/home".fsType or null) == "btrfs")) {
+      services.snapper = {
+        configs.home = {
+          SUBVOLUME = "/home";
+          ALLOW_USERS = [ "${user}" ];
+          TIMELINE_CREATE = true;
+          TIMELINE_CLEANUP = true;
+          TIMELINE_LIMIT_HOURLY = "10";
+          TIMELINE_LIMIT_DAILY = "10";
+          TIMELINE_LIMIT_WEEKLY = "5";
+          TIMELINE_LIMIT_MONTHLY = "1";
+        };
       };
-    };
-    environment.systemPackages = with pkgs; [
-      btrfs-assistant
-    ];
-  };
+      environment.systemPackages = with pkgs; [
+        btrfs-assistant
+      ];
+    })
+  ];
 }

6494d9750364cb2fab3859aef96b9bf574fc78d6
Luytan <luytan@khora.me>
2026-03-29 16:52:04 +0200

    LACT: Check amdgpu enabled

--- modules/system/services/lact.nix
+++ modules/system/services/lact.nix
@@ -6,12 +6,23 @@
 }:
 let
   cfg = config.modules.services;
+  amdgpuEnabled = config.modules.hardware.graphics.amdgpu;
 in
 {
-  config = lib.mkIf cfg.lact {
-    environment.systemPackages = with pkgs; [ lact ];
-    systemd.packages = with pkgs; [ lact ];
-    systemd.services.lactd.wantedBy = [ "multi-user.target" ];
-    hardware.amdgpu.overdrive.enable = true;
-  };
+  config = lib.mkMerge [
+    (lib.mkIf cfg.lact {
+      assertions = [
+        {
+          assertion = amdgpuEnabled;
+          message = "modules.services.lact requires modules.hardware.graphics.amdgpu = true.";
+        }
+      ];
+    })
+    (lib.mkIf (cfg.lact && amdgpuEnabled) {
+      environment.systemPackages = with pkgs; [ lact ];
+      systemd.packages = with pkgs; [ lact ];
+      systemd.services.lactd.wantedBy = [ "multi-user.target" ];
+      hardware.amdgpu.overdrive.enable = true;
+    })
+  ];
 }

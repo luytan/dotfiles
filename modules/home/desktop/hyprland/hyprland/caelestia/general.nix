1d3ab346e62d243e3e93d17fc5a931d3acc62b28
Luytan <luytan@khora.me>
2026-03-31 17:40:36 +0200

    Hyprland: Formatting and disable scaling on xwayland

--- modules/home/desktop/hyprland/hyprland/caelestia/general.nix
+++ modules/home/desktop/hyprland/hyprland/caelestia/general.nix
@@ -3,12 +3,16 @@ with lib;
 {
   wayland.windowManager.hyprland.settings = {
     # Monitor Configuration
-    monitor = if osConfig.networking.hostName == "glaceon" then [
-      "desc:Thermotrex Corporation TL140ADXP01, 2560x1600@120, 0x0, 1.25, vrr, 1, bitdepth, 10, cm, auto"
-      ", preferred, auto, 1"
-    ] else [
-      ", preferred, auto, 1"
-    ];
+    monitor =
+      if osConfig.networking.hostName == "glaceon" then
+        [
+          "desc:Thermotrex Corporation TL140ADXP01, 2560x1600@120, 0x0, 1.25, vrr, 1, bitdepth, 10, cm, auto"
+          ", preferred, auto, 1"
+        ]
+      else
+        [
+          ", preferred, auto, 1"
+        ];
 
     # General Settings
     general = {
@@ -23,7 +27,9 @@ with lib;
       "col.active_border" = "$activeWindowBorderColour";
       "col.inactive_border" = "$inactiveWindowBorderColour";
     };
-
+    xwayland = {
+      force_zero_scaling = true;
+    };
     # Dwindle Layout
     dwindle = {
       preserve_split = true;

dd1ffd962e3628d40ee572bec1947dac5eede7f2
Luytan <luytan@khora.me>
2026-04-02 13:05:58 +0200

    tlp: Rolled back some changes to fix my dgpu being stuck on D0

--- modules/system/services/tlp.nix
+++ modules/system/services/tlp.nix
@@ -13,18 +13,15 @@ in
     environment.systemPackages = with pkgs; [
       powertop
     ];
-
-    powerManagement.powertop.enable = true;
-
-    services.system76-scheduler.settings.cfsProfiles.enable = true;
     services.upower.enable = true;
+    powerManagement.powertop.enable = true;
+    services.system76-scheduler.settings.cfsProfiles.enable = true;
     services.power-profiles-daemon.enable = false;
-    services.thermald.enable = intelMicrocodeEnabled;
-
+    services.thermald.enable = config.hardware.cpu.intel.updateMicrocode or false;
     services.tlp = {
       enable = true;
       settings = {
-        CPU_BOOST_ON_AC = 0;
+        CPU_BOOST_ON_AC = 1;
         CPU_BOOST_ON_BAT = 0;
         CPU_HWP_DYN_BOOST_ON_AC = 1;
         CPU_HWP_DYN_BOOST_ON_BAT = 0;
@@ -34,20 +31,10 @@ in
         CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
         PLATFORM_PROFILE_ON_AC = "performance";
         PLATFORM_PROFILE_ON_BAT = "quiet";
-        RUNTIME_PM_ON_AC = "on";
-        USB_AUTOSUSPEND = 0;
-
-        WIFI_PWR_ON_AC = "off";
-        WIFI_PWR_ON_BAT = "on";
-
-        SOUND_POWER_SAVE_ON_AC = 0;
-        SOUND_POWER_SAVE_ON_BAT = 1;
-        SOUND_POWER_SAVE_CONTROLLER = "Y";
-
         START_CHARGE_THRESH_BAT0 = 75;
         STOP_CHARGE_THRESH_BAT0 = 81;
+        RUNTIME_PM_ON_AC = "auto";
       };
-
       pd = {
         enable = true;
       };

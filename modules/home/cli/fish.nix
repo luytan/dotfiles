eadf5fdd118a57787bc0992ab0c335559123da81
Luytan <luytan@khora.me>
2026-04-15 20:39:32 +0200

    fish: replace oh my fish for starship

--- modules/home/cli/fish.nix
+++ modules/home/cli/fish.nix
@@ -1,32 +1,16 @@
 { pkgs, ... }:
-let
-  bobthefish = pkgs.fetchFromGitHub {
-    owner = "oh-my-fish";
-    repo = "theme-bobthefish";
-    rev = "c5efbe05aed81b201454c0ae1190ba91ea1970ac";
-    hash = "sha256-12Xd43vy6qQILV/Q5BeoGaul6DsQv5OloCPLXwR6KNU=";
-  };
-in
 {
   programs.fish = {
     enable = true;
     interactiveShellInit = ''
-      set -g fish_greeting
-      set -g theme_powerline_fonts yes
-      set -g theme_nerd_fonts yes
-      set -g theme_display_git yes
-      set -g theme_display_git_dirty yes
-      set -g theme_display_cmd_duration yes
-      set -g theme_display_date no
-      set -g theme_color_scheme gruvbox
       zoxide init fish | source
+      direnv hook fish | source
+      starship init fish | source
     '';
     shellAliases = {
       nix-rebuild = "nh os switch -H $(hostname)";
       nix-rebuild-boot = "nh os boot -H $(hostname)";
       nix-clean = "nix-collect-garbage -d";
-      niri-low = "niri msg output 'Thermotrex Corporation TL140ADXP01 Unknown' mode 1920x1200@60.000";
-      niri-high = "niri msg output 'Thermotrex Corporation TL140ADXP01 Unknown' mode 2560x1600@120.000";
       cd = "z";
       cls = "clear";
       cat = "bat";
@@ -38,13 +22,10 @@ in
         name = "grc";
         src = pkgs.fishPlugins.grc.src;
       }
-      {
-        name = "bobthefish";
-        src = bobthefish;
-      }
     ];
   };
   home.packages = with pkgs; [
     grc
+    starship
   ];
 }

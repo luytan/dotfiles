c6195f9777eb6a279b1b217ee88443e933da14be
Luytan <luytan@khora.me>
2026-04-15 20:39:43 +0200

    security: replace sudo by sudo-rs

--- modules/system/core/security.nix
+++ modules/system/core/security.nix
@@ -6,4 +6,9 @@
   security.polkit.enable = true;
   security.soteria.enable = true;
   security.apparmor.enable = true;
+
+  security.sudo-rs = {
+    enable = true;
+    execWheelOnly = true;
+  };
 }
